d04e3990a8cc7063ae45cd948b657189aad71733
Wolf Oliver <owolf@anynines.com>
2016-11-07 09:24:53 +0100

    make it running on cf (without authentication)

--- start.js
+++ start.js
@@ -2,9 +2,8 @@ const DeepstreamServer = require('deepstream.io')
 const C = DeepstreamServer.constants
 
 const server = new DeepstreamServer({
-  host: 'localhost',
   port: process.env.PORT || 6020,
-  tcpPort: 6021
+  tcpServerEnabled: false
 })
 
 server.set('dataTransforms', [{
